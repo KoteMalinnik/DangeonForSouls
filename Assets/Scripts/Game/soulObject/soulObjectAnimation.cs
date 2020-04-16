0647f01a132a6d2cc13835d5b23b19f83137a4ce
Nikita Kalinnikov <niki9888985@mail.ru>
2020-04-16 04:43:07 +0300

    Небольшие правки объекта soulObject
    Правки кода для удобства восприятия
    Логика переключения видимости soulObject вынесена в другой класс

---
+++ Assets/Scripts/Game/soulObject/soulObjectAnimation.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class soulObjectAnimation : MonoBehaviour
+{
+	/// <summary>
+	/// Свет, прикрепленный к дочернему объекту
+	/// </summary>
+	Light halo;
+	soulObjectVisibilityListener visibilityListener;
+
+	void Awake()
+	{
+		halo = GetComponentInChildren<Light>();
+		visibilityListener = GetComponent<soulObjectVisibilityListener>();
+	}
+
+	[SerializeField, Tooltip("Скорость изменения размера ореола (halo)")]
+	/// <summary>
+	/// Скорость изменения размера ореола (halo)
+	/// </summary>
+	float haloRangeAnimationSpeed = 0.1f;
+
+	[SerializeField, Tooltip("Cкорость анимации движения объекта")]
+	/// <summary>
+	/// Cкорость анимации движения объекта
+	/// </summary>
+	float moveAnimationSpeed = 0.1f;
+
+	float startPositionY;  //начальная позиция объекта по оси Y
+	float haloRangeStartPhase; //начальная фаза для анимации светимости
+
+	void Start()
+	{
+		startPositionY = transform.position.y; //получаем позицию по оси Y
+		haloRangeStartPhase = Random.Range(0, 0.1f); //устанавливаем случайную фазу
+	}
+
+	void Update()
+	{
+		//если объект в зоне видимости камеры
+		if(visibilityListener.visibleStatement)
+		{
+			//Благодаря случайной фазе объекты двигаются по-разному и имеют разные размеры ореолов
+			var newRange = 0.6f + Mathf.PingPong(haloRangeAnimationSpeed * Time.time, haloRangeStartPhase);
+			halo.range = newRange;
+
+			var phaseY = Mathf.PingPong(moveAnimationSpeed * Time.time, haloRangeStartPhase);
+			var newPosition = new Vector3(transform.position.x, startPositionY + phaseY, 0);
+			transform.position = newPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/Game/soulObject/soulObjectVisibilityListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Переключает состояние видимости soulObject
+/// </summary>
+public class soulObjectVisibilityListener : MonoBehaviour
+{
+	/// <summary>
+	/// Состояние видимости объекта
+	/// </summary>
+	public bool visibleStatement { get; private set; } = false;
+
+    void OnBecameVisible()
+	{
+		//объект попал в зону видимости камеры;
+		visibleStatement = true;
+	}
+
+	void OnBecameInvisible()
+	{
+		visibleStatement = false;
+		//объект вышел за пределы видимости камеры
+		Destroy(gameObject);
+	}
+}
