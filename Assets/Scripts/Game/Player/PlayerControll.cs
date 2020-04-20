87ecbeb0cb769df979029bf68f1e08dff911179e
Nikita Kalinnikov <niki9888985@mail.ru>
2020-04-20 04:18:04 +0300

    Рефакторинг PlayerControll. Напоминание

--- Assets/Scripts/Game/Player/PlayerControll.cs
+++ Assets/Scripts/Game/Player/PlayerControll.cs
@@ -7,172 +7,150 @@ using UnityEngine;
 [RequireComponent(typeof(InputController))]
 public class PlayerControll : MonoBehaviour
 {
-//	GameManager gm;
-//	SphereAudioControll sac;
+	public string groundTag = "ground";
 
-//	public static PlayerControll instance { get; private set; }
-//	void Awake()
-//	{
-//		gm = GameManager.instance;
+	public bool isGrounded { get; private set; } //true, если находимся у объекта ground
+	public bool isMoving { get; private set; } = false; //true, если происходит вертикальное перемещение объекта Sphere
 
-//		if (instance == null) instance = this;
-//		else if (instance = this) Destroy(this);
+	bool gameStarted { get; set; } = false; //true, если произошел первый свайп с момента запуска сцены Game
 
-//		gm.pc = instance;
-//		sac = GetComponent<SphereAudioControll>();
-//	}
+	public float animSpeed = 3;
 
-//	void Start()
-//	{
-//		if (pauseGUI_Manager.instance != null) gm.p_gui = pauseGUI_Manager.instance;
-//		else throw new System.Exception("GUI_Manager instance is not exist.");
-//	}
+	[Header("Speed Parametrs")]
+	public float verticalSpeed = 1;
+	public float horizontalSpeed = 1;
+	public float speedCoefficient = 0;
+	public float maxSpeedBoost = 10;
+	float speedBoost = 0;
 
-//	public string groundTag = "ground";
+	public float score = 0;
 
-//	public bool gameOver { get; private set; } = false; //true, если игрок зацепил объект hole
-//	public bool isGrounded { get; private set; } //true, если находимся у объекта ground
-//	public bool isMoving { get; private set; } = false; //true, если происходит вертикальное перемещение объекта Sphere
+	readonly Vector2 mYAxis = new Vector2(0, 1);
 
-//	bool gameStarted { get; set; } = false; //true, если произошел первый свайп с момента запуска сцены Game
+	const float mAngleRange = 30;
+	const float mMinSwipeDist = 10.0f;
+	Vector2 mStartPosition;
 
-//	public float animSpeed = 3;
+	void Update()
+	{
+		if (!gm.p_gui.isPause && !gameOver)
+		{
+			if (gameStarted) score += Time.deltaTime*10; //считаем очки, если игрок начал управлять сферой
 
-//	[Header("Speed Parametrs")]
-//	public float verticalSpeed = 1;
-//	public float horizontalSpeed = 1;
-//	public float speedCoefficient = 0;
-//	public float maxSpeedBoost = 10;
-//	float speedBoost = 0;
+			if (speedBoost <= maxSpeedBoost) speedBoost = score / 100; //рассчитываем дополнительное ускорение
 
-//	public float score = 0;
+			//горизонтальное перемещение
+			Vector3 pos = transform.position;
+			pos = new Vector3(pos.x + (horizontalSpeed + speedCoefficient * speedBoost) * Time.deltaTime, pos.y, pos.z);
+			transform.position = pos;
 
-//	readonly Vector2 mYAxis = new Vector2(0, 1);
+			//если игрок не перемещается вертикально
+			if (!isMoving)
+			{
+#if !UNITY_EDITOR
+				//управление свайпом
+				if(Input.touchCount>0)
+				{
+					if (Input.GetTouch(0).phase == TouchPhase.Began)
+					{
+						mStartPosition = new Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+					}
 
-//	const float mAngleRange = 30;
-//	const float mMinSwipeDist = 10.0f;
-//	Vector2 mStartPosition;
+					if (Input.GetTouch(0).phase == TouchPhase.Moved)
+					{
+						Vector2 endPosition = new Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+						Vector2 swipeVector = endPosition - mStartPosition;
 
-//	void Update()
-//	{
-//		if (!gm.p_gui.isPause && !gameOver)
-//		{
-//			if (gameStarted) score += Time.deltaTime*10; //считаем очки, если игрок начал управлять сферой
+						if (swipeVector.magnitude > mMinSwipeDist)
+						{
+							swipeVector.Normalize();
 
-//			if (speedBoost <= maxSpeedBoost) speedBoost = score / 100; //рассчитываем дополнительное ускорение
+							float angleOfSwipe = Vector2.Dot(swipeVector, mYAxis);
+							angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
 
-//			//горизонтальное перемещение
-//			Vector3 pos = transform.position;
-//			pos = new Vector3(pos.x + (horizontalSpeed + speedCoefficient * speedBoost) * Time.deltaTime, pos.y, pos.z);
-//			transform.position = pos;
+							if (angleOfSwipe < mAngleRange && transform.position.y <= 0)
+							{
+								if (!gameStarted) gameStarted = true;
 
-//			//если игрок не перемещается вертикально
-//			if (!isMoving)
-//			{
-//#if !UNITY_EDITOR
-//				//управление свайпом
-//				if(Input.touchCount>0)
-//				{
-//					if (Input.GetTouch(0).phase == TouchPhase.Began)
-//					{
-//						mStartPosition = new Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
-//					}
+								isGrounded = false;
+								OnSwipeTop();
+							}
+							else if ((180.0f - angleOfSwipe) < mAngleRange && transform.position.y >= 0)
+							{
+								if (!gameStarted) gameStarted = true;
 
-//					if (Input.GetTouch(0).phase == TouchPhase.Moved)
-//					{
-//						Vector2 endPosition = new Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
-//						Vector2 swipeVector = endPosition - mStartPosition;
+								isGrounded = false;
+								OnSwipeBottom();
+							}
+						}
+					}
+				}
+#elif UNITY_EDITOR
+				//управление из редактора
+				if(Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y <= 0)
+				{
+					if (!gameStarted) gameStarted = true;
 
-//						if (swipeVector.magnitude > mMinSwipeDist)
-//						{
-//							swipeVector.Normalize();
+					isGrounded = false;
+					OnSwipeTop();
+				}
 
-//							float angleOfSwipe = Vector2.Dot(swipeVector, mYAxis);
-//							angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
+				if(Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y >= 0)
+				{
+					if (!gameStarted) gameStarted = true;
 
-//							if (angleOfSwipe < mAngleRange && transform.position.y <= 0)
-//							{
-//								if (!gameStarted) gameStarted = true;
+					isGrounded = false;
+					OnSwipeBottom();
+				}
 
-//								isGrounded = false;
-//								OnSwipeTop();
-//							}
-//							else if ((180.0f - angleOfSwipe) < mAngleRange && transform.position.y >= 0)
-//							{
-//								if (!gameStarted) gameStarted = true;
+				if(Input.GetKeyDown(KeyCode.Escape))
+				{
+					gm.p_gui.pauseFunc();
+				}
+#endif
+			}
+		}
+	}
 
-//								isGrounded = false;
-//								OnSwipeBottom();
-//							}
-//						}
-//					}
-//				}
-//#elif UNITY_EDITOR
-//				//управление из редактора
-//				if(Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y <= 0)
-//				{
-//					if (!gameStarted) gameStarted = true;
+	//функция вызывается при свайпе вверх
+	void OnSwipeTop()
+	{
+		StartCoroutine(moveSphere(1));
+		//Debug.Log("Swipe Top");
+	}
 
-//					isGrounded = false;
-//					OnSwipeTop();
-//				}
+	//функция вызывается при свайпе вниз
+	void OnSwipeBottom()
+	{
+		StartCoroutine(moveSphere(-1));
+		//Debug.Log("Swipe Bottom");
+	}
 
-//				if(Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y >= 0)
-//				{
-//					if (!gameStarted) gameStarted = true;
+	//корутина вертикального движения. direction = 1 - вверх, -1 - вниз
+	IEnumerator moveSphere(int direction)
+	{
+		isMoving = true; //выставление переменной, которая блокирует управление вертикальным перемещением
+		Vector3 pos = transform.position;
 
-//					isGrounded = false;
-//					OnSwipeBottom();
-//				}
+		//пока объект не коснулся ground или hole
+		while (!isGrounded && !gameOver)
+		{
+			//если игра не на паузе
+			if(!gm.p_gui.isPause)
+			{
+				if (verticalSpeed <= 0) throw new System.Exception("Vertical speed = 0.");
+				//рассчет вертикально-горизонтального движния
+				pos = new Vector3(pos.x + (horizontalSpeed + speedCoefficient * speedBoost) * Time.deltaTime, pos.y + verticalSpeed * direction * Time.deltaTime, pos.z);
 
-//				if(Input.GetKeyDown(KeyCode.Escape))
-//				{
-//					gm.p_gui.pauseFunc();
-//				}
-//#endif
-//			}
-//		}
-//	}
+				//выравнивание, чтобы не западало на платформу
+				if ((pos.y > 4 && direction > 0) || (pos.y < -4 && direction < 0)) pos = new Vector3(pos.x, 4 * direction, pos.z);
+				transform.position = pos;
+			}
+			yield return new WaitForEndOfFrame();
+		}
+		isMoving = false; //вертикальное движение завершено
 
-//	//функция вызывается при свайпе вверх
-//	void OnSwipeTop()
-//	{
-//		StartCoroutine(moveSphere(1));
-//		//Debug.Log("Swipe Top");
-//	}
-
-//	//функция вызывается при свайпе вниз
-//	void OnSwipeBottom()
-//	{
-//		StartCoroutine(moveSphere(-1));
-//		//Debug.Log("Swipe Bottom");
-//	}
-
-//	//корутина вертикального движения. direction = 1 - вверх, -1 - вниз
-//	IEnumerator moveSphere(int direction)
-//	{
-//		isMoving = true; //выставление переменной, которая блокирует управление вертикальным перемещением
-//		Vector3 pos = transform.position;
-
-//		//пока объект не коснулся ground или hole
-//		while (!isGrounded && !gameOver)
-//		{
-//			//если игра не на паузе
-//			if(!gm.p_gui.isPause)
-//			{
-//				if (verticalSpeed <= 0) throw new System.Exception("Vertical speed = 0.");
-//				//рассчет вертикально-горизонтального движния
-//				pos = new Vector3(pos.x + (horizontalSpeed + speedCoefficient * speedBoost) * Time.deltaTime, pos.y + verticalSpeed * direction * Time.deltaTime, pos.z);
-
-//				//выравнивание, чтобы не западало на платформу
-//				if ((pos.y > 4 && direction > 0) || (pos.y < -4 && direction < 0)) pos = new Vector3(pos.x, 4 * direction, pos.z);
-//				transform.position = pos;
-//			}
-//			yield return new WaitForEndOfFrame();
-//		}
-//		isMoving = false; //вертикальное движение завершено
-
-//		//Debug.Log("End of sphere moving");
-//		yield return null;
-//	}
+		//Debug.Log("End of sphere moving");
+		yield return null;
+	}
 }
